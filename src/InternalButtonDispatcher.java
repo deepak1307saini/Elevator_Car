2e2f0397d43c5b5e38fb2afa48fdee4df740ecd6
deepaK1307saini <deepak1307saini@gmail.com>
2023-09-03 17:11:33 +0530

    InternalButtonDispatcher class created

---
+++ src/InternalButtonDispatcher.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class InternalButtonDispatcher {
+    List<ElevatorController> elevatorControllers;
+
+    public void submitInternalRequest(int floor, ElevatorCar elevatorCar){
+
+    }
+}
