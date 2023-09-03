ad1f7a31c7f388b6e0354b03be5ceb385e8b8cd2
deepaK1307saini <deepak1307saini@gmail.com>
2023-09-03 19:43:52 +0530

    Some functionalities added in ElevatorCar

--- src/ElevatorCar.java
+++ src/ElevatorCar.java
@@ -17,6 +17,45 @@ public class ElevatorCar {
         currentFloor = 0;
     }
 
+    public void showDisplay() {
+        elevatorDisplay.showDisplay();
+    }
+
+    public void pressButton(int destination) {
+        internalButtons.pressButton(destination, this);
+    }
+
+    public void setDisplay() {
+        this.elevatorDisplay.setDisplay(currentFloor, direction);
+    }
+
+    boolean moveElevator(Direction dir, int destinationFloor){
+        int startFloor = currentFloor;
+        if(dir == Direction.UP) {
+            for(int i = startFloor; i<=destinationFloor; i++) {
+
+                this.currentFloor = startFloor;
+                setDisplay();
+                showDisplay();
+                if(i == destinationFloor) {
+                    return true;
+                }
+            }
+        }
+
+        if(dir == Direction.DOWN) {
+            for(int i = startFloor; i>=destinationFloor; i--) {
+
+                this.currentFloor = startFloor;
+                setDisplay();
+                showDisplay();
+                if(i == destinationFloor) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
 
 
